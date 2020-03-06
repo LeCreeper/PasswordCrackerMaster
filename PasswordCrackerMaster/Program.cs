a65a5fbda40b2cf7e3c457e802035d46f279bac0
Rasmus Drotved Laursen <rasm781y@edu.easj.dk>
2020-03-06 13:15:33 +0100

    UDP works Two-Way

--- PasswordCrackerMaster/Program.cs
+++ PasswordCrackerMaster/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
 namespace PasswordCrackerMaster
 {
@@ -6,7 +10,55 @@ namespace PasswordCrackerMaster
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region UDPServer
+
+            //UdpClients
+            UdpClient udpMasterServer = new UdpClient(7899);
+            
+            UdpClient udpSlaveClient = new UdpClient("192.168.24.186",7999);
+
+            //IP's
+            IPAddress slaveIp = IPAddress.Parse("192.168.24.186");
+            IPAddress ownIp = IPAddress.Parse("192.168.24.178");
+
+            //EndPoints
+            IPEndPoint IpEndPoint = new IPEndPoint(slaveIp, 7899);
+            
+
+            try
+            {
+                Console.WriteLine("Server is waiting...");
+
+                while (true)
+                {
+                    //Sending Data
+                    string test = "yes!";
+
+                    Console.WriteLine("Ready to send...");
+                    Byte[] sendData = Encoding.ASCII.GetBytes("Data is: " + test);
+                    Console.WriteLine("Data is: " + test);
+                    udpSlaveClient.Send(sendData, sendData.Length);
+                    Console.WriteLine("Data is sent!");
+
+                    Thread.Sleep(2000);
+
+                    //Receiving Data
+                    Console.WriteLine("Ready to receive...");
+                    Byte[] receivedRawData = udpMasterServer.Receive(ref IpEndPoint);
+                    string receivedStringData = Encoding.ASCII.GetString(receivedRawData);
+
+                    Console.WriteLine("The data reads: " + receivedStringData);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+            }
+
+            #endregion
+
+
         }
     }
 }
